1b98e56d960f1147b980056d0147c7e51ee5e0b6
hjjung <taotao0@gmail.com>
2021-01-20 17:06:04 +0900

    modify : notify(jicofo) disable in interface_config.js

--- interface_config.js
+++ interface_config.js
@@ -52,7 +52,7 @@ var interfaceConfig = {
 
     DISABLE_DOMINANT_SPEAKER_INDICATOR: true,
 
-    DISABLE_FOCUS_INDICATOR: false,
+    DISABLE_FOCUS_INDICATOR: true,
 
     /**
      * If true, notifications regarding joining/leaving are no longer displayed.
