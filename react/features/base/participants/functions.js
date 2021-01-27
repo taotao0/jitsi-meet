5469ead9ff4c02b98210edd4f902094768eb1176
dh.lee <dhlee.official@gmail.com>
2021-01-27 10:32:15 +0900

    [release-v1.0]
    - native
    	- 참여자 Display name 변경
    	- Moderator인 경우에만 , 모두 음소거 메뉴버튼 렌더링

--- react/features/base/participants/functions.js
+++ react/features/base/participants/functions.js
@@ -183,7 +183,7 @@ export function getParticipantDisplayName(
 
     return typeof interfaceConfig === 'object'
         ? interfaceConfig.DEFAULT_REMOTE_DISPLAY_NAME
-        : 'Fellow Jitster';
+        : 'Guest';
 }
 
 /**
